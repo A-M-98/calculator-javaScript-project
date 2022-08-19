fec89a482b30ee639598649c9724890b2285e034
Afaq Munawar <afaqmunawar12@gmail.com>
2022-08-19 10:28:27 +0100

    html updated and sass completed.

---
+++ script.js
@@ -0,0 +1,12 @@
+// first we need to grab our html elements and store the in variables
+
+//querySelector
+const allClearButton = document.querySelector('.all-clear');
+const clearButton = document.querySelector('.clear');
+const plusMinusButton = document.querySelector('.plus-minus');
+const equalsButton = document.querySelector('.equals');
+const screenNumbers = document.querySelector('.screen-numbers');
+
+//querySelectorAll
+const numberButtons = document.querySelectorAll('.number-btn');
+const functionButtons = document.querySelectorAll('.function-btn');
