f497c8792612528bd5741330938ab9acc74fb9c6
Afaq Munawar <afaqmunawar12@gmail.com>
2022-08-22 15:31:24 +0100

    new js script

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,1 +1,10 @@
 "use strict";
+
+var output = document.getElementById('output');
+var buttons = document.querySelectorAll('.button');
+var equalsButton = document.getElementById('#equals');
+var allClearButton = document.getElementById('#allClear');
+var clearButton = document.getElementById('#clear');
+var percentageButton = document.getElementById('#percentage');
+var operations = document.querySelectorAll('#operation');
+var numbers = document.querySelectorAll('#number');
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let output = document.getElementById('output');
+
+let buttons = document.querySelectorAll('.button');
+const equalsButton = document.getElementById('#equals');
+const allClearButton = document.getElementById('#allClear');
+const clearButton = document.getElementById('#clear');
+const percentageButton = document.getElementById('#percentage');
+const operations = document.querySelectorAll('#operation');
+const numbers = document.querySelectorAll('#number');
 
