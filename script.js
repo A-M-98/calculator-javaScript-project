b3fc2fbcf101ea0f2a9fb611d8e77a866da58c84
Afaq Munawar <afaqmunawar12@gmail.com>
2022-08-22 15:08:52 +0100

    updated html

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// first we need to grab our html elements and store the in variables
 
-//querySelector
-const allClearButton = document.querySelector('.all-clear');
-const clearButton = document.querySelector('.clear');
-const percentageButton = document.querySelector('.percentage');
-const equalsButton = document.querySelector('.equals');
-const screenNumbers = document.querySelector('.screen-numbers');
-
-//querySelectorAll
-const numberButtons = document.querySelectorAll('.number-btn');
-const functionButtons = document.querySelectorAll('.function-btn');
